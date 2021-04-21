9efd3bdd2b14ce2c9017937f916b79ef8ebee8ca
Aleksandr Sosedskyi <oleksandr.susidskyi@nure.ua>
2021-04-21 19:30:04 +0300

    Fix

--- budget_info/budget_info/settings/base.py
+++ budget_info/budget_info/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,3 +128,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
